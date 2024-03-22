3cc739dcc0bf7bbe81b5e9c6ff18eabbb35b1c0f
Felipe Ferreira <felipeferreira0811@gmail.com>
2024-03-22 11:24:28 -0300

    fix: Validações de segurança nos links e atualização CV

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,43 +1,62 @@
-import { Download, Github, Instagram, Linkedin } from 'lucide-react'
-import profilePicture from '../assets/profile.jpg'
-import CV_enUS from '../assets/CV_en.pdf'
-import CV_ptBR from '../assets/CV_pt.pdf'
-import Button from './Button'
+import { Download, Github, Instagram, Linkedin } from "lucide-react";
+import profilePicture from "../assets/profile.jpg";
+import CV_enUS from "../assets/CV_en.pdf";
+import CV_ptBR from "../assets/CV_pt.pdf";
+import Button from "./Button";
 
 export default function Home() {
   return (
-    <div className='w-full flex flex-col text-center items-center justify-center gap-3 mt-16'>
+    <div className="w-full flex flex-col text-center items-center justify-center gap-3 mt-16">
       <img
-        className='rounded-full object-cover size-72'
+        className="rounded-full object-cover size-72"
         src={profilePicture}
-        alt="" />
-      <h2 className='text-4xl font-semibold'>Felipe Ferreira</h2>
-      <p className='text-lg text-violet-200/60'>Software Developer</p>
+        alt=""
+      />
+      <h2 className="text-4xl font-semibold">Felipe Ferreira</h2>
+      <p className="text-lg text-violet-200/60">Software Developer</p>
       <div className="flex gap-2 item">
-        <Button className='border-none bg-violet-800'>
-          <a href="https://www.linkedin.com/in/fs-ferreiraa/" target='_blank' tabIndex={-1}>
+        <Button className="border-none bg-violet-800">
+          <a
+            href="https://www.linkedin.com/in/fs-ferreiraa/"
+            target="_blank"
+            tabIndex={-1}
+            rel="noopener noreferrer"
+          >
             <Linkedin size={28} />
           </a>
         </Button>
-        <Button className='border-none bg-violet-800' >
-          <a href="https://github.com/fs-ferreira" target='_blank' tabIndex={-1}>
+        <Button className="border-none bg-violet-800">
+          <a
+            href="https://github.com/fs-ferreira"
+            target="_blank"
+            tabIndex={-1}
+            rel="noopener noreferrer"
+          >
             <Github size={28} />
           </a>
         </Button>
-        <Button className='border-none bg-violet-800'>
-          <a href="https://www.instagram.com/feli_pih/" target='_blank' tabIndex={-1}>
+        <Button className="border-none bg-violet-800">
+          <a
+            href="https://www.instagram.com/feli_pih/"
+            target="_blank"
+            tabIndex={-1}
+            rel="noopener noreferrer"
+          >
             <Instagram size={28} />
           </a>
         </Button>
       </div>
-      <a href={CV_enUS} download="CV - Felipe da Silva Ferreira" tabIndex={-1} >
-        <Button className='mt-3 font-semibold text-lg hocus:scale-105'>
-          <Download />Download CV
+      <a href={CV_enUS} download="CV - Felipe da Silva Ferreira" tabIndex={-1}>
+        <Button className="mt-3 font-semibold text-lg hocus:scale-105">
+          <Download />
+          Download CV
         </Button>
       </a>
-      <a href={CV_ptBR} download="CV - Felipe da Silva Ferreira" tabIndex={-1} >
-        <Button className='font-semibold border-none hocus:scale-105 hocus:bg-transparent'>or download in pt-BR</Button>
+      <a href={CV_ptBR} download="CV - Felipe da Silva Ferreira" tabIndex={-1}>
+        <Button className="font-semibold border-none hocus:scale-105 hocus:bg-transparent">
+          or download in pt-BR
+        </Button>
       </a>
     </div>
-  )
+  );
 }
--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -1,27 +1,25 @@
 interface ProjectCardProps {
-  imgSrc: any,
-  title: string
-  link: string
+  imgSrc: any;
+  title: string;
+  link: string;
 }
 
 export default function ProjectCard({ imgSrc, title, link }: ProjectCardProps) {
-
   return (
-    <div className='bg-transparent max-w-[440px]'>
-      <button
-        className="w-full outline-none hocus:scale-105 transition-all shadow-md"
-      >
+    <div className="bg-transparent max-w-[440px]">
+      <button className="w-full outline-none hocus:scale-105 transition-all shadow-md">
         <a
           href={`https://github.com/fs-ferreira/${link}`}
           target="_blank"
+          rel="noopener noreferrer"
           tabIndex={-1}
         >
           <img src={imgSrc} alt={title} className="w-full rounded-t-md" />
-          <div className='text-center p-3 text-violet-50 bg-violet-700/80 rounded-b-md'>
-            <span className='font-semibold sm:text-lg'>{title}</span>
+          <div className="text-center p-3 text-violet-50 bg-violet-700/80 rounded-b-md">
+            <span className="font-semibold sm:text-lg">{title}</span>
           </div>
         </a>
       </button>
     </div>
-  )
+  );
 }
